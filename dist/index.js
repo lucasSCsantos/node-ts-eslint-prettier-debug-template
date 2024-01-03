7c67ba17e3a94544c55be315d68ce507aa64b855
lucasSCsantos <luc.cristovam10@gmail.com>
2024-01-03 19:52:18 -0300

    Set first template setup

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3002;
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
